de65cccdf07d6f06c781aef43efe9bea705532e7
Liam Martin <liammatthewmartin@gmail.com>
2025-05-16 17:16:25 +1200

    Set up initial mod

---
+++ src/main/java/com/thelim23/kingbobmod/item/ModItems.java
@@ -0,0 +1,40 @@
+package com.thelim23.kingbobmod.item;
+
+import com.thelim23.kingbobmod.KingBobMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+
+    public static final Item BANANA = registerItem("banana", new Item(new Item.Settings()));
+    public static final Item CHEESE = registerItem("cheese", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(KingBobMod.MOD_ID,  name), item);
+    }
+
+    public static void registerModItems() {
+        KingBobMod.LOGGER.info("Registering Mod Items for " + KingBobMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PINK_GARNET);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(RAW_PINK_GARNET);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(BANANA);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(CHEESE);
+        });
+    }
+}
