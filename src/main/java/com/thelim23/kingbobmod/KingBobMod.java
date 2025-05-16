36c4ee38106438af20e0fea5d09a9c222a32bac8
Liam Martin <liammatthewmartin@gmail.com>
2025-05-16 16:03:01 +1200

    Set up initial mod

---
+++ src/main/java/com/thelim23/kingbobmod/KingBobMod.java
@@ -0,0 +1,16 @@
+package com.thelim23.kingbobmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KingBobMod implements ModInitializer {
+	public static final String MOD_ID = "kingbobmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/thelim23/kingbobmod/KingBobModClient.java
@@ -0,0 +1,10 @@
+package com.thelim23.kingbobmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KingBobModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
