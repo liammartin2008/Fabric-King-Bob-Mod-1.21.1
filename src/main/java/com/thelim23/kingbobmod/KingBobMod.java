82f8e195994d486ba341cfc484b2aa55bb50ff8d
Liam Martin <liammatthewmartin@gmail.com>
2025-05-16 17:16:25 +1200

    Added the following items: * Pink Garnet * Raw Pink Garnet * Banana * Cheese

--- src/main/java/com/thelim23/kingbobmod/KingBobMod.java
+++ src/main/java/com/thelim23/kingbobmod/KingBobMod.java
@@ -1,5 +1,6 @@
 package com.thelim23.kingbobmod;
 
+import com.thelim23.kingbobmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class KingBobMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
